55599a92bcb571ee999df2158bd48fca5e4eda63
Gertjan De Wilde <info@gertjandewilde.com>
2020-05-01 02:37:06 +0200

    Update package.json

--- shared/components/header.component.tsx
+++ shared/components/header.component.tsx
@@ -20,7 +20,7 @@ const Header: FunctionComponent<Props> = ({}) => {
           <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div> */}
-      <div className="w-full block text-sm flex-grow lg:flex lg:items-center justify-end lg:content-end lg:w-auto">
+      <div className="w-full lg:block text-sm flex-grow lg:flex lg:items-center justify-end lg:content-end lg:w-auto hidden">
         {NAV_LINKS.map(item => {
           if (item.type === 'button') {
             return <a href={item.href} key={item.key} className={classNames('inline-block px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0', item.className)}>{item.label}</a>
